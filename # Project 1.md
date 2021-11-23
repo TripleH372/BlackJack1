127c53b99ba9f2330ce9d9ae79a514e01493ef72
Sid Meka <gsmekarrow@gmail.com>
2021-11-23 15:59:13 -0500

    :q exit() exit quit ls

---
+++ # Project 1.md
@@ -0,0 +1,28 @@
+# Project 1
+## Game Schematics
+###### There will be a dealer and a bettor
+###### The player will be the bettor
+###### The dealer will be operated by computer using the Random function to choose card values from an array.
+###### The way the cards are dealt wiil be stored as an Array.
+###### Popping will be used to take out of the deck of cards.
+###### Pushing will be used pushed into either the Dealer's or Bettor's hand, whomever's turn it is.
+###### The deck will be reset after each hand or round is played.
+## Basic Rules
+###### There will be two cards given to both the Bettor and Dealer.
+###### The Bettor will only see one of the two cards the Dealer has been given.
+###### The goal is to get 21 without busting (going over 21).
+###### The Bettor goes first with hitting or standing.
+###### Hitting means that the Bettor will take another card.
+###### Standing means that the Bettor will stop taking additional cards.
+###### An ace will be either a one or an eleven (whichever benefits the player with that deck).
+###### A 2-9 will count as the number is shown
+###### A 10, K, Q, and J will all count as 10.
+## Scoring
+###### Whatever amount the player bets will be used as a temporary score (your money).
+###### If the Bettor wins the round, he or she will take all the score he or she betted doubled.
+###### For example, if the Bettor were to bet 200 points, then he or she would take back 400 points.
+###### If the Bettor loses the round, he or she will lose the score, and have to bet again.
+## Entering Numbers
+###### Keypad will be used for the Bettor.
+###### The Bettor will not be able to bet MORE than the amount of total score (money) he or she has.
+###### ONLY INTS
---
+++ script.js
@@ -0,0 +1,207 @@
+// Started Code in Python
+// Drew immediate code and transferred from Aphrx's (YouTuber) Python code.
+// Changed a little code by being inspired from here but didn't copy anything: https://www.thatsoftwaredude.com/content/6417/how-to-code-blackjack-using-javascript
+let kemp=0;
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+let vals = ['Ace', 'King', 'Queen', 'Jack',
+  'Ten', 'Nine', 'Eight', 'Seven', 'Six',
+  'Five', 'Four', 'Three', 'Two'
+];
+
+let textArea = document.getElementById('textArea');
+let newGameButton = document.getElementById('newGameButton');
+let hitButton = document.getElementById('hitButton');
+let standButton = document.getElementById('standButton');
+
+hitButton.style.display = 'none';
+standButton.style.display = 'none';
+
+let gameStarted = false;
+let isItOver = false;
+let playerWon = false;
+let dealerCards = [];
+let playerCards = [];
+let dealerScore = 0;
+let playerScore = 0;
+let deck = [];
+
+newGameButton.addEventListener('click', function() {
+//got inspiration from https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener
+  gameStarted = true;
+  isItOver = false;
+  playerWon = false;
+
+  deck = creatorX();
+  shuffleDeck(deck);
+  dealerCards = [shiftDeck(), shiftDeck()];
+  playerCards = [shiftDeck(), shiftDeck()];
+  newGameButton.style.display = 'none';
+  hitButton.style.display = 'inline-block';
+  standButton.style.display = 'inline-block';
+  showStatus();
+})
+
+function creatorX() {
+  let deck = []
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < vals.length; j++) {
+      let card = {
+        suit: suits[i],
+        value: vals[j]
+      }
+      deck.push(card);
+    }
+  }
+  return deck;
+}
+
+function shuffleDeck(deck){
+  for(let i=0; i<deck.length; i++)
+  {
+    let swapper = Math.floor(Math.random() *deck.length);
+    let tmp = deck[swapper];
+    deck[swapper] = deck[i];
+    deck[i] = tmp; 
+  }
+}
+
+hitButton.addEventListener('click', function(){
+  playerCards.push(shiftDeck());
+  checkDone();
+  showStatus();
+});
+
+standButton.addEventListener('click', function(){
+  isItOver = true;
+  checkDone();
+  showStatus();
+});
+
+function checkDone(){
+  uss();
+  
+  if(isItOver){
+    while(dealerScore<playerScore &&
+          playerScore <=21 &&
+          dealerScore <=21){
+            dealerCards.push(shiftDeck());
+            uss();
+    }
+  }
+    
+    if(playerScore>21){
+      playerWon=false;
+      isItOver = true;
+    }
+    
+    else if(dealerScore>21){
+      playerWon = true;
+      isItOver = true;
+    }
+    
+    else if(isItOver){
+      if(playerScore>dealerScore){
+        playerWon = true;
+      }
+      else{
+        playerWon = false;
+      }
+    }
+}
+
+function getCardString(card) {
+  let n=[card.value, card.suit];
+  return n.join(" of ");
+}
+function getCardNumericValue(card){
+  switch(card.value){
+    case 'Ace':
+      return 1;
+    case 'Two':
+      return 2;
+    case 'Three':
+      return 3;
+    case 'Four':
+      return 4;
+    case 'Five':
+      return 5;
+    case 'Six':
+      return 6;
+    case 'Seven':
+      return 7;
+    case 'Eight':
+      return 8;
+    case 'Nine':
+      return 9;
+    default:
+      return 10; 
+  }
+}
+function showStatus()
+{
+  if(gameStarted==false)
+  {
+    textArea.innerText = 'Welcome to Blackjack!';
+    return; 
+  }
+  
+  let dealerCardString = '';
+  for(let i=0; i<dealerCards.length; i++)
+  {
+    dealerCardString =dealerCardString+ getCardString(dealerCards[i]) + '\n';
+  }
+  let playerCardString='';
+  for(let i=0; i<playerCards.length; i++)
+  {
+    playerCardString =playerCardString+ getCardString(playerCards[i]) + '\n';
+  }
+  
+  uss();
+  
+  textArea.innerText = 'Dealer :\n' + dealerCardString + '(score: ' + dealerScore + ')\n\n' +
+                        
+                        'Player :\n' + playerCardString + 
+                        '(score: ' + playerScore + ')\n\n';
+  let string1=textArea.innerText;
+                        
+  if(isItOver){
+    if(playerScore==dealerScore) textArea.innerText=string1+"Push!";
+    else{if(playerWon)
+    {
+      textArea.innerText=string1+"You won!";
+    }
+    else{
+      textArea.innerText=string1+"You lost!";
+    }}
+    newGameButton.style.display = 'inline';
+    hitButton.style.display = 'none';
+    standButton.style.display = 'none';
+    
+  }
+}
+
+function gs(a){
+  let score = 0;
+  let aceChecker = false;
+  for(let i=0; i<a.length; i++){
+    let card = a[i];
+    score=score+ getCardNumericValue(card);
+    if(card.value == 'Ace'){
+      aceChecker = true;
+    }
+    
+    if(aceChecker && score+10<=21) return score+10;
+    //Playing this game through, I seem to have mathematical issues whenever cards are added when an Ace is in play.
+  }
+   /*else*/return score; 
+}
+
+function uss(){
+  dealerScore = gs(dealerCards);
+  playerScore = gs(playerCards); 
+}
+
+
+function shiftDeck() {
+  return deck.shift();
+}
